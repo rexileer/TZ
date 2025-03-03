6d8ef60541456b0fce2e9e4070fbc16aa2a8cfe9
rexileer <110720074+rexileer@users.noreply.github.com>
2025-03-03 20:26:59 +0300

    Broadcast db

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Broadcast
 
-# Register your models here.
+@admin.register(Broadcast)
+class BroadcastAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "sent", "message")
--- newsletters/models.py
+++ newsletters/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Broadcast(models.Model):
+    message = models.TextField("Текст сообщения")
+    created_at = models.DateTimeField(auto_now_add=True)
+    sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Рассылка #{self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
